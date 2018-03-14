efd54911dee9dccfdee87c466e23d61bf62a6129
arifin09 <kadenzo.ilham@gmail.com>
2018-03-14 18:34:42 +0700

    add show
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   src/resources/assets/js/components/bantenprov/kegiatan/Kegiatan.index.vue
    	modified:   src/resources/assets/js/components/bantenprov/kegiatan/Kegiatan.show.vue

--- src/resources/assets/js/components/bantenprov/kegiatan/Kegiatan.show.vue
+++ src/resources/assets/js/components/bantenprov/kegiatan/Kegiatan.show.vue
@@ -1,31 +1,115 @@
 <template>
-  <div>
+  <div class="card">
+    <div class="card-header">
+      <i class="fa fa-table" aria-hidden="true"></i> Show Kegiatan {{ model.label }}
 
-    <div class="card mb-3">
-      <div class="card-header">Grafik Kegiatan</div>
-      <div class="card-body">
-        <echarts-kegiatan></echarts-kegiatan>
-      </div><!-- /.card-body -->
-    </div><!-- /.card -->
-
-    <div class="row">
-      <div class="col-md-6">
-        <div class="card mb-3">
-          <div class="card-header">Grafik Kota</div>
-          <div class="card-body">
-            <echarts-kegiatan-kota></echarts-kegiatan-kota>
-          </div><!-- /.card-body -->
-        </div><!-- /.card -->
-      </div>
-      <div class="col-md-6">
-        <div class="card mb-3">
-          <div class="card-header">Grafik Tahunan</div>
-          <div class="card-body">
-            <echarts-kegiatan-tahun></echarts-kegiatan-tahun>
-          </div><!-- /.card-body -->
-        </div><!-- /.card -->
-      </div>
+      <ul class="nav nav-pills card-header-pills pull-right">
+        <li class="nav-item">
+          <button class="btn btn-primary btn-sm" role="button" @click="back">
+            <i class="fa fa-arrow-left" aria-hidden="true"></i>
+          </button>
+        </li>
+      </ul>
     </div>
 
+    <div class="card-body">
+      <vue-form class="form-horizontal form-validation" :state="state" @submit.prevent="onSubmit">
+        <div class="form-row">
+          <div class="col-md">
+            <b>Label :</b> {{ model.label }}
+          </div>
+        </div>
+
+        <div class="form-row mt-4">
+          <div class="col-md">
+            <b>Description :</b> {{ model.description }}
+          </div>
+        </div>
+
+        
+        
+      </vue-form>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    axios.get('api/kegiatan/' + this.$route.params.id)
+      .then(response => {
+        if (response.data.status == true) {
+          this.model.label = response.data.kegiatan.label;
+          this.model.old_label = response.data.kegiatan.label;
+          this.model.description = response.data.kegiatan.description;
+        } else {
+          alert('Failed');
+        }
+      })
+      .catch(function(response) {
+        alert('Break');
+        window.location.href = '#/admin/kegiatan';
+      })
+
+  },
+  data() {
+    return {
+      state: {},
+      model: {
+        label: "",
+        description: "",
+        pendaftaran: "",
+      },
+      pendaftaran: []
+    }
+  },
+  methods: {
+    onSubmit: function() {
+      let app = this;
+
+      if (this.state.$invalid) {
+        return;
+      } else {
+        axios.put('api/siswa/' + this.$route.params.id, {
+            label: this.model.label,
+            description: this.model.description,
+            old_label: this.model.old_label,
+            pendaftaran_id: this.model.pendaftaran.id
+          })
+          .then(response => {
+            if (response.data.status == true) {
+              if(response.data.message == 'success'){
+                alert(response.data.message);
+                app.back();
+              }else{
+                alert(response.data.message);
+              }
+            } else {
+              alert(response.data.message);
+            }
+          })
+          .catch(function(response) {
+            alert('Break ' + response.data.message);
+          });
+      }
+    },
+    reset() {
+      axios.get('api/siswa/' + this.$route.params.id + '/edit')
+        .then(response => {
+          if (response.data.status == true) {
+            this.model.label = response.data.siswa.label;
+            this.model.description = response.data.siswa.description;
+          } else {
+            alert('Failed');
+          }
+        })
+        .catch(function(response) {
+          alert('Break ');
+        });
+    },
+    back() {
+      window.location = '#/admin/kegiatan';
+    }
+  }
+}
+</script>
